df00cd773de7caaa6e14e6268027e9e7cc41316a
minilu7 <nastua24@mail.ru>
2022-05-17 15:47:48 +0300

    dodelano

--- dz lesson9/task26.py
+++ dz lesson9/task26.py
@@ -14,12 +14,15 @@
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 df = pd.DataFrame({
     'Страна': ['Россия', 'Казахстан', 'Украина', 'Белоруссия', 'Узбекистан'],
     '2017 год': [1665, 179, 131, 60, 50],
     '2018 год': [1702, 182, 154, 63, 58],
 }, index=['RU', 'KZ', 'UA', 'BY', 'UZ']
 )
+df.plot(kind='barh', y='2017 год', color='red')
+plt.show()
 
-print(df)
 
